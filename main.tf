6358ae0f36f2741b9d8c8f5b64ab9602af4ea639
Jose Maria Servian Rivas <jservian@ESX5CG0110NVS.es.kworld.kpmg.com>
2021-10-18 14:50:56 +0200

    change to dynamic block

--- main.tf
+++ main.tf
@@ -29,5 +29,12 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name                           = azurerm_virtual_network.vnet.name
   enforce_private_link_endpoint_network_policies = lookup(var.subnet_enforce_private_link_endpoint_network_policies, var.subnet_names[count.index], false)
   service_endpoints                              = lookup(var.subnet_service_endpoints, var.subnet_names[count.index], [])
-  delegation                                     = lookup(local.subnet_delegation, var.subnet_names[count.index], [])
+
+  dynamic "delegation"{
+    for_each = lookup(local.subnet_delegation, var.subnet_names[count.index], [])
+    content {
+      name = delegation.value["name"]
+      service_delegation = delegation.value["service_delegation"]
+    }
+  }
 }
